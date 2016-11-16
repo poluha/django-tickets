2bcf8dfa512e36e2f3cef035e53cfe4f1ecc775d
pol_uha <pol_uha@rambler.ru>
2016-11-16 03:34:07 +0300

    1

--- tickets/urls.py
+++ tickets/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth.decorators import login_required
 from tickets.views import MyTicketListView, MyTicketDetailView, TicketCreateView
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^my/$', login_required(MyTicketListView.as_view()), name='list'),
     url(r'^my/(?P<pk>\d+)/$', login_required(MyTicketDetailView.as_view()), name='detail'),
     url(r'^create/$', login_required(TicketCreateView.as_view()), name='create'),
-)
+]
